190eaf4483a41a90fa95416fb408415143a3c58e
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-22 01:37:36 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /var/www/html/
-docker-compose -f docker-compose.yaml down
+
+# Change directory to the deployment directory
+cd /var/www/html/ || exit
+
+# Check if docker-compose.yaml exists and run the docker-compose command
+if [ -f docker-compose.yaml ]; then
+    docker-compose down
+else
+    echo "docker-compose.yaml not found in /var/www/html/"
+    exit 1
+fi
