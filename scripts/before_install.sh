5902dcd11cdb73fd54cfd013638f48ce9eea81bf
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-22 01:44:15 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Print current working directory
+echo "Current directory: $(pwd)"
+
 # Create the directory if it doesn't exist
 mkdir -p /var/www/html/
 
 # Copy files to the target directory
 cp -r /home/ec2-user/super-app/* /var/www/html/
+
+# Verify if the files are copied correctly
+echo "Files in /var/www/html/:"
+ls -l /var/www/html/
