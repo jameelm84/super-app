a108466cc43ae364155d23a4c8a29f1bb903e659
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-22 12:59:19 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-
 # Print current working directory
 echo "Current directory: $(pwd)"
-
-# Verify if the directory exists
-if [ -d /var/www/html/ ]; then
-    cd /var/www/html/
-    echo "Changed to directory: $(pwd)"
-else
-    echo "Directory /var/www/html/ does not exist."
-    exit 1
-fi
-
-# Check if docker-compose.yaml exists and run the docker-compose command
-if [ -f docker-compose.yaml ]; then
-    echo "docker-compose.yaml found, stopping containers..."
-    docker-compose down
-else
-    echo "docker-compose.yaml not found in /var/www/html/"
-    exit 1
-fi
+# Stop the application
+cd /var/www/html/
+docker-compose down
