9aeaa29b2b558768981159757211a54cfb5216c0
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-23 01:49:04 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Print current working directory
-echo "Current directory: $(pwd)"
-# Create the directory if it doesn't exist
-mkdir -p /home/ec2-user/super-app
+cd /var/www/html
+docker-compose down
