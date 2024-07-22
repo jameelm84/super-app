6f74d3450d448b2103ec6a329d2b8fff2d8505ed
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-23 02:29:45 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Print current working directory
-echo "Current directory: $(pwd)"
-# Copy files to the target directory
-cp -r /home/ec2-user/super-app/* /var/www/html/
+cd /home/ec2-user/super-app
+docker-compose build
