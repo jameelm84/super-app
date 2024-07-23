cf85c8e595c9f2195020706c56391cae06442b3d
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-23 17:42:53 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-cd /home/ec2-user/super-app
-docker-compose down
+
+# בדוק אם התיקייה קיימת לפני שאתה מנסה לגשת אליה
+if [ -d "/var/www/html/" ]; then
+  cd /var/www/html/
+else
+  echo "/var/www/html/ does not exist"
+  exit 1
+fi
+
+# בדוק אם הקובץ קיים לפני שאתה מנסה להשתמש בו
+if [ -f "./docker-compose.yaml" ]; then
+  docker-compose down
+else
+  echo "docker-compose.yaml does not exist in /var/www/html/"
+  exit 1
+fi
