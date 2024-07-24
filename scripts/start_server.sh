2ac345bfb8dc691a83c886cef20cf67c4d4ad312
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-24 19:51:32 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# הפעלת הקונטיינרים של docker-compose
-cd /var/www/html/ || exit 1
-
-if [ -f "docker-compose.yaml" ]; then
-  docker-compose up -d
-else
-  echo "docker-compose.yaml not found in /var/www/html/"
-  exit 1
-fi
+cd /var/www/html || exit 1
+docker-compose up -d
