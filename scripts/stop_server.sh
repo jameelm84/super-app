db7618d848eb531cad5476825af28ef27d2cf133
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-24 19:50:36 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-# עצירת הקונטיינרים של docker-compose
-cd /var/www/html/ || exit 1
-
-# בדוק אם קובץ docker-compose.yaml קיים לפני הרצת docker-compose down
-if [ -f "docker-compose.yaml" ];then
-  docker-compose down
-else
-  echo "docker-compose.yaml not found in /var/www/html/"
-  exit 1
-fi
+cd /var/www/html || exit 1
+docker-compose down
