6bca1197bdbb07baaff971df8f7088014c33b3f2
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-24 03:25:09 +0300

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# בדוק אם התיקייה קיימת לפני שאתה מנסה לגשת אליה
-if [ -d "/var/www/html/" ]; then
-  cd /var/www/html/
-else
-  echo "/var/www/html/ does not exist"
-  exit 1
+# Create the directory if it does not exist
+if [ ! -d "/var/www/html/" ]; then
+  sudo mkdir -p /var/www/html/
 fi
 
-# בדוק אם הקובץ קיים לפני שאתה מנסה להשתמש בו
-if [ -f "./docker-compose.yaml" ]; then
+cd /var/www/html/ || exit 1
+
+# Check if docker-compose.yaml exists before running docker-compose down
+if [ -f "docker-compose.yaml" ]; then
   docker-compose down
 else
-  echo "docker-compose.yaml does not exist in /var/www/html/"
+  echo "docker-compose.yaml not found in /var/www/html/"
   exit 1
 fi
