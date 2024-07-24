bbf993b8fe8507f57acedf32016df696b8a2d80b
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-24 19:23:04 +0300

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# בדוק אם הקובץ docker-compose.yaml קיים בנתיב הנכון
-if [ -f "/var/www/html/docker-compose.yaml" ]; then
-  echo "Validation passed: docker-compose.yaml exists"
+# בדיקת סטטוס של השירות
+service_status=$(docker-compose ps -q | xargs docker inspect -f '{{ .State.Status }}' | grep -v "running")
+if [ -z "$service_status" ]; then
+  echo "All services are running."
 else
-  echo "Validation failed: docker-compose.yaml does not exist"
-  exit 1
-fi
-
-# בדוק אם התהליך רץ
-if pgrep -f "docker-compose"; then
-  echo "Validation passed: docker-compose is running"
-else
-  echo "Validation failed: docker-compose is not running"
+  echo "Some services are not running."
   exit 1
 fi
