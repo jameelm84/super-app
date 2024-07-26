3d4d0722e1039f699bb8629e8a9e67075ebdcff3
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-26 04:23:32 +0300

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# בדיקת תקינות השירותים
-cd /home/ec2-user/super-app || exit
-docker ps
-
-if ! curl -sSf http://localhost:3000 > /dev/null; then
-    echo "Node.js service is not responding"
-    exit 1
-fi
-
-if ! curl -sSf http://localhost:80 > /dev/null; then
-    echo "PHP service is not responding"
-    exit 1
-fi
-
-echo "All services are running and responsive"
+# Validation commands to ensure the service is up and running
+curl -f http://localhost:3000
