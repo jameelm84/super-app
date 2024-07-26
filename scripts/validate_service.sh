cbde81aeda080de9035f86cb7aacf9170b539eca
Jameel Mahagna <139367814+jameelm84@users.noreply.github.com>
2024-07-26 03:21:53 +0300

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-# בדוק אם כל הקונטיינרים רצים
-if ! docker ps | grep -q 'super-app_node-app_1'; then
-    echo "Node.js app is not running"
-    exit 1
-fi
+# בדיקת תקינות השירותים
+cd /home/ec2-user/super-app || exit
+docker ps
 
-if ! docker ps | grep -q 'super-app_php-app_1'; then
-    echo "PHP app is not running"
-    exit 1
-fi
-
-if ! docker ps | grep -q 'super-app_mysql_1'; then
-    echo "MySQL is not running"
-    exit 1
-fi
-
-# בדוק אם ה-Node.js שירות זמין
 if ! curl -sSf http://localhost:3000 > /dev/null; then
     echo "Node.js service is not responding"
     exit 1
 fi
 
-# בדוק אם ה-PHP שירות זמין
 if ! curl -sSf http://localhost:80 > /dev/null; then
     echo "PHP service is not responding"
     exit 1
